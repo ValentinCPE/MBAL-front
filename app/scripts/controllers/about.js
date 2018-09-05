a376884093cdb8a0cd35f193915ad86534b13dbb
Valentin Guevara <valentin.guevara@etu.univ-lyon1.fr>
2018-09-05 19:05:28 +0200

    front initialization

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mbalFrontApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mbalFrontApp
+ */
+angular.module('mbalFrontApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
